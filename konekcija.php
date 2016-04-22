5222708bbd69bd05f6bc66ee794a2cfc30882838
Milos <milos.medic.130.14@ict.edu.rs>
2016-04-22 22:19:09 +0200

    Delete konekcija.php

--- konekcija.php
+++
@@ -1,9 +0,0 @@
-<?php
-	@mysql_set_charset('utf8');
-	@$conn = mysql_connect('localhost', 'root', '');
-	$database = mysql_select_db('root', $conn);
-	
-	if (!$conn) {
-		die('Veza sa serverom baze podataka nije uspostavljena! ');
-	}
-?> 
