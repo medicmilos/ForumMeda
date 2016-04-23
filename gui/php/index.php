97973320129918c2825d755f71012af4dbe7bc85
Milos Medic <medamilosmedic@gmail.com>
2016-04-23 19:49:33 +0200

    index i css

---
+++ gui/php/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+	<title>Meda - Forum</title>
+		<meta http-equiv="Content-type" content="text/html; charset=utf-8"/>
+		<meta name="description" content=""/>
+		<meta name="keywords" content=""/>
+		<meta name="author" content=""/>
+		<link rel="shortcut icon" href=""/>
+		<link rel="stylesheet" type="text/css" href=""/>
+		<script type="text/javascript" src="style.css"></script>
+</head>
+	<body>
+		
+	</body>
+</html>
