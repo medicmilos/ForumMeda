8cb76cb5e60ecdd83f48cfd201098c136e082fd3
Milos Medic <medamilosmedic@gmail.com>
2016-04-23 22:38:33 +0200

    sitne izmene

--- gui/php/footer.php
+++ gui/php/footer.php
@@ -1,26 +1,26 @@
-
-
 <div id="footer">
-    
     <div id="gore">
         <div id="gore2">
-        <h3>ForumMeda</h3>
-        
-         <div id="meni_footer">
-            <ul>
-                <li><a href="">About us /</a></li>
-                <li><a href="">Most popular posts /</a></li>
-                <li><a href="">Top Posters /</a></li>
-                <li><a href="">Archive /</a></li>
-                <li><a href="">Contact us</a></li>
-            </ul>
-        </div>
+			<div id="dole3">
+				<a href="#"><h3>ForumMeda</h3><br/><br/>
+				<p>With you since Oluja 1995.</p></a>
+			</div>
+			
+			<div id="meni_footer">
+				<ul>
+					<li><a href="">Home </a></li> &sol;
+					<li><a href="">Contact </a></li> &sol;
+					<li><a href="">Author </a></li> &sol;
+					<li><a href="">About </a></li> &sol;
+					<li><a href="">Dokumentacija </a></li>
+				</ul>
+			</div>
         </div>
     </div>
    
     <div id="dole">
         <div id="dole2">
-        <p>Copyright &copy; MedaFORUM. All rights reserved.</p>
+        <p>Copyright &copy; <a href="#">ForumMeda</a>. All rights reserved.</p>
         
         <div id="mreze">
             <a href=""><img src=""></a>
@@ -31,5 +31,4 @@
         </div>
         </div>
     </div>
-    
 </div>
