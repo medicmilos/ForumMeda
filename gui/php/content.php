23ed1a6ce57da7fb67ce05e9e1d353abd70e54e9
GoksiiLija <goran.uruk96@gmail.com>
2016-04-24 22:58:08 +0200

    prepravka
    gogili :D

--- gui/php/content.php
+++ gui/php/content.php
@@ -1,6 +1,37 @@
 
 	<div id="sadrzaj">
-		content php
+	
+	<!-- PHP posle obradjuje ovo-->
+		<div class='sadrzaj_paket'>
+			<div class='paket_levo'>
+				<div class='paket_levo_glasovi'>
+					<span class='paket_levo_glasovi_broj'>1</span>
+					<span class='paket_levo_glasovi_tekst'>votes</span>
+				</div>
+				<div class='paket_levo_glasovi'>
+					<span class='paket_levo_glasovi_broj'>2</span>
+					<span class='paket_levo_glasovi_tekst'>answer</span>
+				</div>
+				<div class='paket_levo_glasovi'>
+					<span class='paket_levo_glasovi_broj'>3</span>
+					<span class='paket_levo_glasovi_tekst'>views</span>
+				</div>
+			</div>
+			<div class='paket_desno'>
+				<div class='paket_desno_naslov'>how to recursive load n children in comment table when lazy loading and proxies is false?</div>
+				<div class='paket_desno_tagovi'>
+					<span class='paket_desno_tagovi_tag'>css</span>
+					<span class='paket_desno_tagovi_tag'>html</span>
+				</div>
+				<div class='paket_desno_opis'>
+					<span class='paket_desno_opis_time'>answered 3 mins ago by</span>
+					<span class='paket_desno_opis_user'>Mikelis Baltruks</span>
+				</div>
+			</div>
+		</div>
+		<div class='cisti'></div>
+		
+		
 	</div>
 	<div id="desno">
 			<?php
