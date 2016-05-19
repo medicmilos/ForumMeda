112ea1226aa31c0acd2119ba7e51d81a91019fb2
Milos Medic <medamilosmedic@gmail.com>
2016-05-19 15:00:54 +0200

    Revert "resen problem sa loginom"
    This reverts commit 62c883e4cf711d76f66d579f43c1396475645388.

--- gui/php/login.php
+++ gui/php/login.php
@@ -11,25 +11,24 @@
         //$password = mysql_real_escape_string($konekcija, $password);
  
         //$password = md5($password);
-		if(!($username == '' && $password == '')){
-			$upit = "SELECT * FROM users WHERE username='$username' LIMIT 1";
-				include("konekcija.php");	
-					$rezultat = mysql_query($upit, $konekcija);
-				mysql_close($konekcija); 
+		
+		$upit = "SELECT * FROM users WHERE username='$username' LIMIT 1";
+			include("konekcija.php");	
+				$rezultat = mysql_query($upit, $konekcija);
+			mysql_close($konekcija); 
 
-				$id = '';
-				$db_password = '';
-				while($red = mysql_fetch_array($rezultat)){
-					$id = $red['id_users'];
-					$db_password = $red['password'];	
-				}
-				if($password == $db_password) {
-						$_SESSION['username'] = $username;
-						$_SESSION['id_users'] = $id;
-						header("location:index.php");
-				} else { 
-					header("location:register.php?message= <div id='erori'> Login failed!</div>");
-				}
-		}
+			$id = '';
+			$db_password = '';
+			while($red = mysql_fetch_array($rezultat)){
+				$id = $red['id_users'];
+				$db_password = $red['password'];	
+			}
+			if($password == $db_password) {
+					$_SESSION['username'] = $username;
+					$_SESSION['id_users'] = $id;
+					header("location:index.php");
+			} else { 
+				header("location:register.php?message= <div id='erori'> Login failed!</div>");
+			}
 	}
 ?>
